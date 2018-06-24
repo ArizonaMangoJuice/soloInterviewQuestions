72a2b1b3922c614268fce93c9c705c28aeeb2895
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-06-24 17:44:11 -0600

    added boilerplate

---
+++ soloDoubleLinkedList.js
@@ -0,0 +1,14 @@
+class _Node{
+  constructor(data, next, prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class DoubleLinkedList{
+  constructor{
+    this.head = null;
+    this.tail = null;
+  }
+}
